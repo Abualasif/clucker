a0106ceb22f2af72ff53f671dc955f6af8875103
Rahi Al-Asif <abu.al-asif@kcl.ac.uk>
2022-11-13 23:04:50 +0000

    "Add username unit tests and requirements.txt"

--- microblogs/models.py
+++ microblogs/models.py
@@ -1,7 +1,16 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.core.validators import RegexValidator
 
 # Create your models here.
 
 class User(AbstractUser):
+    username = models.CharField(
+        max_length=30, 
+        unique=True,
+        validators=[RegexValidator(
+            regex=r'^@\w{3,}$',
+            message='Username must consist of an @ followed by at least 3 alphanumericals'
+        )]
+    )
     bio = models.TextField()
